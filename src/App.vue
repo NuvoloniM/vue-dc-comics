90653634bd9ad71c9a0d40d5b083b17185d4d29c
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-19 16:39:26 +0200

    ComicsComp

--- src/App.vue
+++ src/App.vue
@@ -43,16 +43,10 @@ export default {
 
 @import './assets/style/variables.scss';
 
-body {
+* {
+  padding: 0;
   margin: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  color: #2c3e50;
+  box-sizing: border-box;
 }
 
 .footer-menu {
---
+++ src/components/ComicsComp.vue
@@ -0,0 +1,37 @@
+<template>
+    <li>
+        <a href="#">
+        <img :src="img" alt="#">
+        <h4> {{ title }}</h4>
+        </a>
+    </li>
+</template>
+<script>
+export default {
+    name: 'ComicsComp',
+    props: {
+        img: String,
+        title: String,
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+@import '../assets/style/mixin.scss';
+li {
+    flex-basis: calc( 100% / 6 );
+    padding-right: 10px;
+    margin: 10px 0;
+    img {
+        width: 100%;
+    }
+    a{
+        text-decoration: none;
+        color: #fff;
+    }
+    h4 {
+        text-transform: uppercase;
+    }
+}
+
+</style>
